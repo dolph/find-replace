487e9bff0745dd0af883b354c4ac5a839535e736
Dolph Mathews <dolph.mathews@gmail.com>
2022-02-06 13:51:36 -0600

    test for ReplaceContents passing

--- find_replace_test.go
+++ find_replace_test.go
@@ -1,6 +1,60 @@
 package main
 
-import "testing"
+import (
+	"log"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestReplaceContents(t *testing.T) {
+	initial := "alpha"
+
+	f, err := os.CreateTemp("", "*")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.Remove(f.Name())
+
+	file_info, err := f.Stat()
+	if err != nil {
+		log.Fatal(err)
+	}
+	baseName := file_info.Name()
+
+	if _, err := f.Write([]byte(initial)); err != nil {
+		log.Fatal(err)
+	}
+	if err := f.Close(); err != nil {
+		log.Fatal(err)
+	}
+
+	dirName := filepath.Dir(f.Name())
+
+	// There has to be a better way to get `f_info` directly for `f`?
+	files, err := os.ReadDir(dirName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	f_info := files[0]
+	for _, file := range files {
+		if file.Name() == baseName {
+			f_info = file
+			break
+		}
+	}
+
+	find := "ph"
+	replace := "f"
+	want := "alfa"
+	fr := findReplace{find: "ph", replace: "f"}
+	fr.ReplaceContents(dirName, f_info)
+
+	got := readFile(f.Name())
+	if got != want {
+		t.Errorf("replace %v with %v in %v, but got %v; want %v", find, replace, initial, got, want)
+	}
+}
 
 func TestRandomStringLengthNegativeOne(t *testing.T) {
 	got := randomString(-1)
