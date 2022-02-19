69bc3dde5947111434a3a5f34c78c91b7f5fe0ab
Dolph Mathews <dolph.mathews@gmail.com>
2022-02-18 23:11:15 -0600

    add test for walkdir

--- find_replace_test.go
+++ find_replace_test.go
@@ -63,6 +63,103 @@ func assertPathExistsAfterRename(t *testing.T, oldPath string, newPath string) {
 	}
 }
 
+func TestWalkDir(t *testing.T) {
+	find := "wh"
+	replace := "f"
+
+	d := createTestDir("", "*")
+	defer os.Remove(d.Path)
+
+	// d1: who/
+	d1 := createTestDir(d.Path, "who")
+	defer os.Remove(d1.Path)
+
+	// d1d1: who/what/
+	d1d1 := createTestDir(d1.Path, "what")
+	defer os.Remove(d1d1.Path)
+
+	// d1d1f1: who/what/when (contains "where")
+	d1d1f1Contents := "where"
+	d1d1f1 := createTestFile(d1d1.Path, "when", d1d1f1Contents)
+	defer os.Remove(d1d1f1.Path)
+
+	// d2: what/
+	d2 := createTestDir(d.Path, "what")
+	defer os.Remove(d2.Path)
+
+	// d2d1: what/when/
+	d2d1 := createTestDir(d2.Path, "when")
+	defer os.Remove(d2d1.Path)
+
+	// d2d1d1: what/when/where (directories with no files)
+	d2d1d1 := createTestDir(d2d1.Path, "where")
+	defer os.Remove(d2d1d1.Path)
+
+	// d3: when/
+	d3 := createTestDir(d.Path, "when")
+	defer os.Remove(d3.Path)
+
+	// d3f1: when/where (contains "why")
+	d3f1Contents := "why"
+	d3f1 := createTestFile(d3.Path, "where", d3f1Contents)
+	defer os.Remove(d3f1.Path)
+
+	// d4: where/ (empty directory in base dir)
+	d4 := createTestDir(d.Path, "where")
+	defer os.Remove(d4.Path)
+
+	// f1: why (file in base dir contains "wh")
+	f1Contents := "wh\nwh\nwh\n"
+	f1 := createTestFile(d.Path, "why", f1Contents)
+	defer os.Remove(f1.Path)
+
+	fr := findReplace{find: find, replace: replace}
+	fr.WalkDir(d)
+
+	// d1: who/ > fo/
+	d1ExpectedPath := d1.Dir() + string(os.PathSeparator) + strings.Replace(d1.Base(), find, replace, -1)
+	assertPathExistsAfterRename(t, d1.Path, d1ExpectedPath)
+
+	// d1d1: who/what/ > fo/foat/
+	d1d1ExpectedPath := d1ExpectedPath + string(os.PathSeparator) + strings.Replace(d1d1.Base(), find, replace, -1)
+	assertPathExistsAfterRename(t, d1d1.Path, d1d1ExpectedPath)
+
+	// d1d1f1: who/what/when > fo/fat/fen (contains "fere")
+	d1d1f1ExpectedPath := d1d1ExpectedPath + string(os.PathSeparator) + strings.Replace(d1d1f1.Base(), find, replace, -1)
+	assertPathExistsAfterRename(t, d1d1f1.Path, d1d1f1ExpectedPath)
+	assertNewContentsOfFile(t, d1d1f1ExpectedPath, d1d1f1Contents, find, replace, "fere")
+
+	// d2: what/ > fat/
+	d2ExpectedPath := d2.Dir() + string(os.PathSeparator) + strings.Replace(d2.Base(), find, replace, -1)
+	assertPathExistsAfterRename(t, d2.Path, d2ExpectedPath)
+
+	// d2d1: what/when/
+	d2d1ExpectedPath := d2ExpectedPath + string(os.PathSeparator) + strings.Replace(d2d1.Base(), find, replace, -1)
+	assertPathExistsAfterRename(t, d2d1.Path, d2d1ExpectedPath)
+
+	// d2d1d1: what/when/where (directories with no files)
+	d2d1d1ExpectedPath := d2d1ExpectedPath + string(os.PathSeparator) + strings.Replace(d2d1d1.Base(), find, replace, -1)
+	assertPathExistsAfterRename(t, d2d1d1.Path, d2d1d1ExpectedPath)
+
+	// d3: when/
+	d3ExpectedPath := d3.Dir() + string(os.PathSeparator) + strings.Replace(d3.Base(), find, replace, -1)
+	assertPathExistsAfterRename(t, d3.Path, d3ExpectedPath)
+
+	// d3f1: when/where (contains "why")
+	d3f1ExpectedPath := d3ExpectedPath + string(os.PathSeparator) + strings.Replace(d3f1.Base(), find, replace, -1)
+	assertPathExistsAfterRename(t, d3f1.Path, d3f1ExpectedPath)
+	assertNewContentsOfFile(t, d3f1ExpectedPath, d3f1Contents, find, replace, "fy")
+
+	// d4: where/ (empty directory in base dir)
+	d4ExpectedPath := d4.Dir() + string(os.PathSeparator) + strings.Replace(d4.Base(), find, replace, -1)
+	assertPathExistsAfterRename(t, d4.Path, d4ExpectedPath)
+
+	// f1: why (file in base dir contains "wh\nwh\nwh\n")
+	f1ExpectedPath := f1.Dir() + string(os.PathSeparator) + strings.Replace(f1.Base(), find, replace, -1)
+	assertPathExistsAfterRename(t, f1.Path, f1ExpectedPath)
+	assertNewContentsOfFile(t, f1ExpectedPath, f1Contents, find, replace, "f\nf\nf\n")
+}
+
 func TestHandleFileWithDir(t *testing.T) {
 	initial := "alpha"
 	find := "ph"
